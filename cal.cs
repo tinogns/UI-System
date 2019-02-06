d0bdd6febe470616eed7fa30685818fe2f0e781e
tinogns <47379274+tinogns@users.noreply.github.com>
2019-02-06 16:00:35 +0700

    tin

---
+++ cal.cs
@@ -0,0 +1,23 @@
+using System;
+ 
+namespace ComSciDev.Com.CSharp.MultiplicationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+            
+            int mul = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+             
+            
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine("  " + mul + " x " + i + " = " + mul * i);
+            }
+ 
+            Console.ReadLine();
+        }
+    }
+}
